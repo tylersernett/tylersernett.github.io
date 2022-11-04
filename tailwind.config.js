c15cbf348c35ec8c9c8cdd40ac6c055cc3e743a1
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-11-04 15:06:49 -0400

    Initialize tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        blue: "#2cbce9",
+        red: "#DC4492",
+        yellow: "#fdcc49",
+        grey: "#ededed",
+        "deep-blue": "#010026",
+        "dark-grey": "#757575",
+        "opaque-black": "rgba(0,0,0,0.35)"
+      },
+      backgroundImage: (theme) => ({
+        "gradient-rainbow":
+          "linear-gradient(81.66deg, #00b5ee 7.21%, #ff45a4 45.05%, #ffba00 78.07%",
+        "gradient-rainblue":
+          "linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)",
+      }),
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+        opensans: ["Open Sans", "sans-serif"],
+      },
+      content: {
+        brush: "url('./assets/brush.png')",
+        person1: "url('./assets/person-1.png')",
+        person2: "url('./assets/person-2.png')",
+        person3: "url('./assets/person-3.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
