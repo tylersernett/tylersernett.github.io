1eb4d21e989cc3a4196cd4ea41572ccab8f868bb
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-11-04 15:49:35 -0400

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scenes/Navbar";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
+
   return (
-    <div className="App">
+    <div className="app bg-deep-blue">
+      <Navbar 
+      selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
     </div>
   );
 }
