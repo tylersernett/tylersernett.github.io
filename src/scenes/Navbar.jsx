5a6382b38d31c7f6d98dfb1be885cc4a18ece5de
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-11-06 18:38:25 -0500

    Add Navbar for desktop & mobile

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -15,22 +15,53 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
     )
 }
 
-const Navbar = ({ selectedPage, setSelectedPage }) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
     const [isMenuToggled, setIsMenuToggled] = useState();
     const isAboveSmallScreens = useMediaQuery('(min-width: 768px)');
+    const navbarBackground = isTopOfPage? "" : "bg-red";
+    
     return (
-        <nav className={`z-40 w-full fixed top-0 py-6`}>
+        <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
             <div className="flex items-center justify-between mx-auto w-5/6">
                 <h4 className="font-playfair text-3xl font-bold">TSJ</h4>
                 {/* DESKTOP NAV */}
                 {isAboveSmallScreens ? (
                     <div className='flex justify-between gap-16 font-opensans text-sm font-semibold'>
+                        {/* TODO: cleanup -- make this component, keep DRY, useContext */}
                         <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                        <Link page="Skills" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                        <Link page="Projects" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                        <Link page="Testimonials" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                        <Link page="Contact" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
                     </div>
                 ) : (
-                    <div></div>
+                    // SMALL SCREEN NAV
+                    <button className='rounded-full bg-red p-2' onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                        <img alt="menu-icon" src='../assets/menu-icon.svg'></img>
+                    </button>
                 )}
-            </div>
+
+                {/* MOBILE MENU POPUP */}
+                {!isAboveSmallScreens && isMenuToggled && (
+                    <div className='fixed right-0 bottom-0 h-full bg-blue w-[300px]'>
+                        {/* CLOSE ICON */}
+                        <div className='flex justify-end p-12'>
+                            <button onClick={() => setIsMenuToggled(false)}>
+                                <img alt="close-icon" src='../assets/close-icon.svg'></img>
+                            </button>
+                        </div>
+
+                        {/* MENU ITEMS */}
+                        <div className='flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue '>
+                            <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                            <Link page="Skills" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                            <Link page="Projects" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                            <Link page="Testimonials" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                            <Link page="Contact" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                        </div>
+                    </div>
+                )}
+                    </div>
         </nav>
     )
 }
