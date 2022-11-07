a2ab3dbbd173d45909abacf8ab7f49cd72efe687
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-11-06 23:05:52 -0500

    Add SocialMediaIcon component

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import useMediaQuery from '../hooks/useMediaQuery'
 import { motion } from 'framer-motion'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import SocialMediaIcons from '../components/SocialMediaIcons'
 
 const Landing = ({ setSelectedPage }) => {
     const isAboveLargeScreens = useMediaQuery('(min-width: 1060px)')
@@ -83,6 +84,18 @@ const Landing = ({ setSelectedPage }) => {
                         <div className='bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center
                         font-playfair px-10'>Let's talk.</div>
                     </AnchorLink>
+                </motion.div>
+                <motion.div
+                className='flex mt-5 justify-center md:justify-start'
+                initial='hidden'
+                whileInView='visible'
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.4, duration: 0.5 }}
+                variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: { opacity: 1, x: 0 }
+                }}>
+                    <SocialMediaIcons />
 
                 </motion.div>
             </div>
