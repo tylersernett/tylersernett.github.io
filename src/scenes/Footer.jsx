8aa2b6fa48bcd6bb80041106cc30decbf9fd149a
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-11-20 17:24:32 -0500

    Tweak style: left-align for md+ screens & adjust py

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -2,10 +2,10 @@ import SocialMediaIcons from "../components/SocialMediaIcons"
 
 const Footer = () => {
     return (
-        <footer className="h-64 bg-red pt-10">
+        <footer className=" bg-red pt-4 pb-12">
             <div className="w-5/6 mx-auto">
                 <SocialMediaIcons />
-                <div className="md:flex justify-center md:justify-between text-center">
+                <div className="md:text-left  text-center">
                     <p className="font-playfair font-semibold text-2xl text-yellow mt-2">TYLER JOHNSON</p>
                     <p className="font-playfair text-md">© 2022 TSJ<br />All Rights Reserved</p>
                 </div>
