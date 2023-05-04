2e01c3d6a0bf1d4ff2b3ec4c48bc5254e3c3ab9e
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-05-04 15:00:43 -0400

    Add Personal Portfolio project

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -19,47 +19,60 @@ const projectsArr = [
     {
         name: 'The Hood eStore',
         description: 'Digital storefront for local Las Cruces, NM business specializing in jewelry, art, and events.',
+        stack: ['React', 'Redux', 'Material UI', 'Strapi', 'Stripe', 'Formik', 'Yup'],
         live: 'https://tylersernett.github.io/react-ecommerce/',
         code: 'https://github.com/tylersernett/react-ecommerce'
     },
     {
         name: 'JSTetris',
         description: 'Minimalist remake of the NES Tetris port with color-blind accessible design.',
+        stack: ['React', 'Redux', 'Material UI', 'Strapi', 'Stripe', 'Formik', 'Yup'],
         live: 'https://tylersernett.github.io/tetris-javascript/',
         code: 'https://github.com/tylersernett/tetris-javascript'
     },
     {
         name: 'Notflix',
         description: 'Front end clone of the Netflix UI built with Tailwind styling.',
+        stack: ['React', 'Redux', 'Material UI', 'Strapi', 'Stripe', 'Formik', 'Yup'],
         live: 'https://tylersernett.github.io/netflixclone/',
         code: 'https://github.com/tylersernett/netflixclone'
     },
     {
         name: 'GRE Dashboard',
         description: 'Accessible front end recreation of the GRE testing interface including a test timer and a calculator.',
+        stack: ['React', 'Redux', 'Material UI', 'Strapi', 'Stripe', 'Formik', 'Yup'],
         live: 'https://tylersernett.github.io/grecalc/',
         code: 'https://github.com/tylersernett/grecalc'
     },
     {
         name: 'Hex Color Trainer',
         description: 'Responsive mini-game where users guess the hexadecimal code of the displayed color.',
+        stack: ['React', 'Redux', 'Material UI', 'Strapi', 'Stripe', 'Formik', 'Yup'],
         live: 'https://tylersernett.github.io/hexcolorgame/',
         code: 'https://github.com/tylersernett/hexcolorgame'
+    },
+    {
+        name: 'Personal Portfolio',
+        description: "View the code of this site.",
+        stack: ['React', 'Redux', 'Material UI', 'Strapi', 'Stripe', 'Formik', 'Yup'],
+        live: '#0',
+        code: 'https://github.com/tylersernett/personal-portfolio'
     },
 ];
 
 const Project = ({ projectObj }) => {
-    const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 
-        bg-grey z-30 flex flex-col justify-center items-center text-center p-8 text-deep-blue`;
+    const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-95 transition duration-500 
+        bg-grey z-30 flex flex-col justify-center items-center text-center py-8 px-4 text-deep-blue`;
     const projectTitle = projectObj.name.split(" ").join("-").toLowerCase();
     return (
         <motion.div variants={projectVariant} className='relative'>
             <div className={overlayStyles}>
-                <p className='text-2xl font-playfair'>{projectObj.name}</p>
-                <p className='mt-5'>
+                <p className='text-xl lg:text-2xl font-playfair'>{projectObj.name}</p>
+                <p className='mt-5 text-sm lg:text-base'>
                     {projectObj.description}
                 </p>
-                <span className='mt-5'><a className='cursor-pointer' href={projectObj.live}>live</a> | <a className='cursor-pointer' href={projectObj.code}>code</a></span>
+                
+                <span className='mt-5 text-sm lg:text-base'><a className='cursor-pointer underline' href={projectObj.live}>live</a> | <a className='cursor-pointer underline' href={projectObj.code}>code</a></span>
             </div>
             <div className='flex justify-center text-center items-center
                         max-w-[400px] max-h-[400px]'>
@@ -108,16 +121,16 @@ const Projects = () => {
                     viewport={{ once: true, amount: 0.2 }} //bug: high amount will prevent project animations from firing on small screens
                     variants={container}
                 >
-                    <div className='flex justify-center text-center items-center p-10 bg-blue 
+                    {/* <div className='flex justify-center text-center items-center p-10 bg-blue 
                         max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold'
                     >
                         BEAUTIFUL USER INTERFACES
-                    </div>
+                    </div> */}
 
                     {projectsArr.map((project) => (
                         <Project projectObj={project} key={project.name} />
                     ))}
-                    
+
                     {/* <div className='flex justify-center text-center items-center p-10 bg-yellow 
                         max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold'
                     >
