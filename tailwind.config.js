1d87be70753c2fdde1739a902241b1becdc88b56
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-05-07 19:55:53 -0400

    Tweak colors to improve a11y

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,18 @@ module.exports = {
     extend: {
       colors: {
         blue: "#2cbce9",
-        red: "#DC4492",
-        yellow: "#fdcc49",
-        grey: "#ededed",
+        red: "#dc2d74",
+        yellow: "#fdcd49",
         "deep-blue": "#010026",
+        grey: "#ededed",
         "dark-grey": "#757575",
-        "opaque-black": "rgba(0,0,0,0.35)"
+        "opaque-black": "rgba(0,0,0,0.35)",
       },
       backgroundImage: (theme) => ({
         "gradient-rainbow":
-          "linear-gradient(81.66deg, #00b5ee 7.21%, #ff45a4 45.05%, #ffba00 78.07%",
+          "linear-gradient(81.66deg, #00b5ee 7%, #ff45a4 45%, #ffba00 78%",
         "gradient-rainblue":
-          "linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)",
+          "linear-gradient(90deg, #24CBFF 14%, #dc2d74 49%, #FFBD0C 107%)",
       }),
       fontFamily: {
         playfair: ["Playfair Display", "serif"],
@@ -26,9 +26,6 @@ module.exports = {
       // vars below allow for config using BEFORE tag
       content: {
         brush: "url('./assets/brush.png')",
-        person1: "url('./assets/brush.png')",
-        person2: "url('./assets/brush.png')",
-        person3: "url('./assets/brush.png')",
       },
     },
     screens: {
