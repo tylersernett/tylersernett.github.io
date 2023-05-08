2155c21d414d5e93d2655e30b2f5269be400511c
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-05-08 03:13:37 -0400

    Hide image for < md screens

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -27,17 +27,13 @@ const MySkills = () => {
                         I'm always open to learning new technologies and I have completed projects using the following:
                     </p>
                 </motion.div>
-                <div className='mt-16 md:mt-0'>
-                    {isAboveMediumScreens ? (
+                <div className='mt-16 md:mt-0 hidden md:block'>
                         <div //add blue offset frame
                             className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
                             before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]"
                         >
                             <img alt="skills" className="z-10" src="assets/skills.png" />
                         </div>
-                    ) : (
-                        <img alt="skills" className="z-10" src="assets/skills.png" />
-                    )}
                 </div>
             </div>
             {/* SKILLS */}
